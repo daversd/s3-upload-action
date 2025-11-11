c0f15cd971c42c1960593cd141b318ffb1314dc8
David Dória <dave_rsd@hotmail.com>
2025-11-11 11:24:47 -0300

    remove errors

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,6 @@ const AWS_ACCESS_KEY_ID = process.env['AWS_ACCESS_KEY_ID'] || '';
 const AWS_SECRET_ACCESS_KEY = process.env['AWS_SECRET_ACCESS_KEY'] || '';
 const AWS_BUCKET = process.env['AWS_BUCKET'] || '';
 
-if (!AWS_ACCESS_KEY_ID) throw new Error('AWS Access Key ID was not provided');
-if (!AWS_SECRET_ACCESS_KEY) throw new Error('AWS Secret Access Keys was not provided');
-if (!AWS_BUCKET) throw new Error('AWS Bucket was not provided');
-
 let input: InputOptions;
 if (NODE_ENV !== 'local') {
   input = {
